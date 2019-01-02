68db117767a76ee8c64c794f59c4364415a77d46
RyosukeMaruyama <maruyama_ryosuke@infonic.co.jp>
2019-01-02 12:04:24 +0900

    Add build task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const cleanCss = require('gulp-clean-css');
 const runSequence = require('run-sequence');
 const rename = require('gulp-rename');
+const replace = require('gulp-replace');
 const gulpAmpValidator = require('gulp-amphtml-validator');
 
 const paths = {
@@ -17,7 +18,7 @@ gulp.task('amphtml:validate', () => {
     return gulp.src(`${paths.src}/index.amp_base.html`)
         .pipe(gulpAmpValidator.validate())
         .pipe(gulpAmpValidator.format())
-        // .pipe(gulpAmpValidator.failAfterError());
+    // .pipe(gulpAmpValidator.failAfterError());
 });
 
 gulp.task('styles', () => {
@@ -25,9 +26,9 @@ gulp.task('styles', () => {
         .pipe(plumber(
             {   // エラー抑制
                 errorHandler: function (err) {
-                console.log(err);
-            }
-        }))
+                    console.log(err);
+                }
+            }))
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCss({ compatibility: 'ie8' })) // cssを圧縮
@@ -64,12 +65,23 @@ gulp.task('inject-styles', function () {
         .pipe(gulp.dest(`${paths.dist}`))
 });
 
-gulp.task('ampdev', gulp.series('styles', 'inject-styles', 'amphtml:validate', (callback) => {
-   callback();
+gulp.task('html-replace', function () {
+    return gulp.src(`${paths.dist}/index.html`)
+        .pipe(replace('src="../images', 'src="images'))
+        .pipe(gulp.dest(`${paths.dist}`))
+});
+
+gulp.task('copy-images', function () {
+    return gulp.src(`images/**`)
+        .pipe(gulp.dest(`${paths.dist}/images`))
+});
+
+gulp.task('ampdev', gulp.series('styles', 'inject-styles', 'amphtml:validate', 'html-replace', 'copy-images', (callback) => {
+    callback();
 }));
 
 gulp.task('default', gulp.series('ampdev', (callback) => {
-    gulp.watch(`${paths.src}/css/**/*.scss`, gulp.series('ampdev'));
+    gulp.watch(`${paths.src}/css/style.scss`, gulp.series('ampdev'));
     gulp.watch(`${paths.src}/index.amp_base.html`, gulp.series('ampdev'));
     callback();
 }));
